f27491e2723b67e0f843f71c68ca059e6f18fe5e
Cristian Villa <c.villavirgen@gmail.com>
2023-06-12 23:27:47 -0600

    Add linters

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const projectTile = document.querySelectorAll(".project-tile");
+const projectTile = document.querySelectorAll('.project-tile');
 
-for (var y = 0; y < projectTile.length; y++) {
-    projectTile[y].addEventListener("mouseover", codeHover);
-    projectTile[y].addEventListener("mouseout", codeOut);
+for (let y = 0; y < projectTile.length; y++) {
+  projectTile[y].addEventListener('mouseover', codeHover);
+  projectTile[y].addEventListener('mouseout', codeOut);
 }
 
 function codeHover() {
-    let code = document.querySelectorAll(".code");
-    
-    for (var i = 0; i < code.length; i++) {
-        code[i].style.color = "orange";
-        code[i].style.transition = "0.3s";
-    }
+  const code = document.querySelectorAll('.code');
+
+  for (let i = 0; i < code.length; i++) {
+    code[i].style.color = 'orange';
+    code[i].style.transition = '0.3s';
+  }
 }
 
 function codeOut() {
-    let code = document.querySelectorAll(".code");
-    
-    for (var i = 0; i < code.length; i++) {
-        code[i].style.color = "#303841";
-    }
+  const code = document.querySelectorAll('.code');
+
+  for (let i = 0; i < code.length; i++) {
+    code[i].style.color = '#303841';
+  }
 }
