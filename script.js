5d15792eda51e533f92e8914c9e04599563ee71c
Cristian Villa <c.villavirgen@gmail.com>
2023-06-12 23:39:37 -0600

    Update js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function codeHover() {
-    const code = document.querySelectorAll('.code');
-    for (let i = 0; i < code.length; i += 1) {
-     code[i].style.color = 'orange';
-     code[i].style.transition = '0.3s';
-    }
-   }
+  const code = document.querySelectorAll('.code');
+  for (let i = 0; i < code.length; i += 1) {
+    code[i].style.color = 'orange';
+    code[i].style.transition = '0.3s';
+  }
+}
    
-   function codeOut() {
-    const code = document.querySelectorAll('.code');
-    for (let i = 0; i < code.length; i += 1) {
-     code[i].style.color = '#303841';
-    }
-   }
+function codeOut() {
+  const code = document.querySelectorAll('.code');
+  for (let i = 0; i < code.length; i += 1) {
+    code[i].style.color = '#303841';
+  }
+}
    
-   const projectTile = document.querySelectorAll('.project-tile');
-   for (let y = 0; y < projectTile.length; y += 1) {
-    projectTile[y].addEventListener('mouseover', codeHover);
-    projectTile[y].addEventListener('mouseout', codeOut);
-   }
+const projectTile = document.querySelectorAll('.project-tile');
+for (let y = 0; y < projectTile.length; y += 1) {
+  projectTile[y].addEventListener('mouseover', codeHover);
+  projectTile[y].addEventListener('mouseout', codeOut);
+}
    
