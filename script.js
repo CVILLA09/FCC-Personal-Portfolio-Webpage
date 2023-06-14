5dae85e2fb359c4771f96d81d17b508aa7b52b83
Cristian Villa <c.villavirgen@gmail.com>
2023-06-13 19:25:44 -0600

    Update html/css/js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function codeHover() {
-  const code = document.querySelectorAll(".code");
+  const code = document.querySelectorAll('.code');
   for (let i = 0; i < code.length; i += 1) {
-    code[i].style.color = "orange";
-    code[i].style.transition = "0.3s";
+    code[i].style.color = 'orange';
+    code[i].style.transition = '0.3s';
   }
 }
 function codeOut() {
-  const code = document.querySelectorAll(".code");
+  const code = document.querySelectorAll('.code');
   for (let i = 0; i < code.length; i += 1) {
-    code[i].style.color = "#303841";
+    code[i].style.color = '#303841';
   }
 }
-const projectTile = document.querySelectorAll(".project-tile");
+const projectTile = document.querySelectorAll('.project-tile');
 for (let y = 0; y < projectTile.length; y += 1) {
-  projectTile[y].addEventListener("mouseover", codeHover);
-  projectTile[y].addEventListener("mouseout", codeOut);
+  projectTile[y].addEventListener('mouseover', codeHover);
+  projectTile[y].addEventListener('mouseout', codeOut);
 }
